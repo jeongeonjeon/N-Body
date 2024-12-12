eea0524491864d0a0d8d99705a47cd3b40caad84
journey <wjddjs901@gmail.com>
2024-12-12 04:18:31 +0000

    1st update

--- Code Skeleton/main.cpp
+++ Code Skeleton/main.cpp
@@ -20,13 +20,24 @@ inline void calcForce(
         double mA, double mB, // masses of body A and body b [in]
         double* Fx, double* Fy, double* Fz // force [out]
         ) {
-            // TODO
+
+    // TODO
+    double dx = pBx - pAx;
+    double dy = pBy - pAy;
+    double dz = pBz - pAz;
+    double r2 = dx * dx + dy * dy + dz * dz + 1e-10; // Add small value to avoid division by zero
+    double r = sqrt(r2);
+    double F = G * mA * mB / r2;
+
+    *Fx = F * (dx / r);
+    *Fy = F * (dy / r);
+    *Fz = F * (dz / r);
 }
 
 #if PLOT
-    void createScatter(double v[][3], double size){
+    void createScatter(double v[][3], double size) {
         std::vector<double> xVec, yVec, zVec;
-        for(int i = 0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             xVec.push_back(v[i][0]);
             yVec.push_back(v[i][1]);
             zVec.push_back(v[i][2]);
@@ -34,7 +45,7 @@ inline void calcForce(
         plt::scatter(xVec, yVec, zVec);
     }
 
-    void displayGraphs(){
+    void displayGraphs() {
         plt::show();
     }
 #endif
@@ -61,8 +72,8 @@ int main() {
 
     auto p = new double[N][3]; // each of the body positions
     double pMean = 0.0, pStdDev = 1.0; // mean and standard deviation of position values
-    std::normal_distribution<double> positionDistribution(pMean,pStdDev);
-    for(int n = 0; n < N; n++) {
+    std::normal_distribution<double> positionDistribution(pMean, pStdDev);
+    for (int n = 0; n < N; n++) {
         p[n][0] = positionDistribution(engine);
         p[n][1] = positionDistribution(engine);
         p[n][2] = positionDistribution(engine);
@@ -74,7 +85,7 @@ int main() {
 
     auto v = new double[N][3]; // each of the body velocities
     double vMean = 0.0, vStdDev = 1.0; // mean and standard deviation of velocity values
-    std::normal_distribution<double> velocityDistribution(vMean,vStdDev);
+    std::normal_distribution<double> velocityDistribution(vMean, vStdDev);
     for (int n = 0; n < N; n++) {
         v[n][0] = velocityDistribution(engine);
         v[n][1] = velocityDistribution(engine);
@@ -86,6 +97,47 @@ int main() {
 
     while (t < T) { // we assume T is defined
         // TODO
+        // Reset forces
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++) {
+            force[i][0] = force[i][1] = force[i][2] = 0.0;
+        }
+
+        // Calculate forces
+        #pragma omp parallel for collapse(2) schedule(dynamic)
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                double Fx, Fy, Fz;
+                calcForce(p[i][0], p[i][1], p[i][2],
+                          p[j][0], p[j][1], p[j][2],
+                          mass[i], mass[j],
+                          &Fx, &Fy, &Fz);
+
+                #pragma omp critical
+                {
+                    force[i][0] += Fx;
+                    force[i][1] += Fy;
+                    force[i][2] += Fz;
+
+                    force[j][0] -= Fx;
+                    force[j][1] -= Fy;
+                    force[j][2] -= Fz;
+                }
+            }
+        }
+
+        // Update velocities and positions
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++) {
+            v[i][0] += dt * force[i][0] / mass[i];
+            v[i][1] += dt * force[i][1] / mass[i];
+            v[i][2] += dt * force[i][2] / mass[i];
+
+            p[i][0] += dt * v[i][0];
+            p[i][1] += dt * v[i][1];
+            p[i][2] += dt * v[i][2];
+        }
+
         t += dt;
     }
 
@@ -95,7 +147,9 @@ int main() {
     printf("Time taken: %f seconds", duration.count()/1000000.0);
 
     #if PLOT
+        std::cout << "Creating scatter plot..." << std::endl;
         createScatter(p, N);  // plot final positions of bodies
+        std::cout << "Displaying plot..." << std::endl;
         displayGraphs(); // show all plots
     #endif
 
@@ -104,8 +158,5 @@ int main() {
     delete[](p);
     delete[](v);
 
+    return 0;
 }
-
-
-
-
